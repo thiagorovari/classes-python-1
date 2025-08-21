f8a710101fcc0b9f3c2732ff7eeeff3feeb54e2b
Thiago <thiagorovari@gmail.com>
2025-08-21 09:22:52 -0300

    First classes

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-class Avaliacao :
-    def __init__(self, matricula=0, presenca=0.1, nota1=0, nota2=0, substitutiva=0, nota_minima=6.0):
-        self.matricula = matricula
-        self._presenca = presenca
-        self.nota1 = nota1
-        self.nota2 = nota2
-        self.substitutiva = substitutiva
-        self.nota_minima = nota_minima
+class Passageiro:
+    def __init__ (self, nome="", rg=""):
+        self.nome=nome
+        self.rg=rg
 
-    @property
-    def presenca(self):
-        return self._presenca
+class Bilhete:
+    def __init__(self, poltrona =0, saida=0, chegada=0, passageiro=None, 
+                 origem=None, destino=None, onibus=None):
+        self.poltrona=poltrona
+        self.saida=saida
+        self.chegada=chegada
+        self.passageiro=passageiro
+        self.origem=origem
+        self.destino=destino
+        self.onibus=onibus
 
-    @presenca.setter
-    def presenca(self, presenca):
-        if(presenca<=1.0):
-            self._presenca=presenca
+class Rodoviaria:
+    def __init__ (self, cidade="", endereco="", guiches=[]):
+        self.cidade=cidade
+        self.endereco=endereco
+        self.guiches=guiches
+
+class Bagagem:
+    def __init__(self, bilhete, identificacao="", peso=0.0):
+        self.bilhete=bilhete
+        self.identificacao=identificacao
+        self.pesso=peso
+
+##class Guiche:
+
         
-
-if __name__ == "__main__":
-    a1 = Avaliacao(123, substitutiva=10.0)
-    a1.presenca = 10.0
-    print(a1.matricula, a1.presenca)
-
-   
