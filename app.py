e9099aaac3de31e393ced7e3352c918bc6b17e8f
Thiago <thiagorovari@gmail.com>
2025-08-21 09:40:01 -0300

    Finishing exercice

--- app.py
+++ app.py
@@ -26,6 +26,35 @@ class Bagagem:
         self.identificacao=identificacao
         self.pesso=peso
 
-##class Guiche:
+class Guiche:
+    def __init__(self, numero=0,area=0.0, empresas=[]):
+        self.numero=numero
+        self.area=area
+        self.empresas=empresas
 
-        
+class Empresa:
+    def __init__(self, nome="", anoFundacao=0, cnpj="", guiches=[]):
+        self.nome=nome
+        self.anoFundacao=anoFundacao
+        self.cnpj=cnpj
+        self.guiches=guiches
+    
+
+class Onibus:
+    def __init__(self, placa="", anoFabricacao=0, bagageiro=0.0, totalAssentos=0, totalLeitos =0, empresa=None):
+        self.placa=placa
+        self.anoFabricacao=anoFabricacao
+        self.bagageiro=bagageiro
+        self.totalAssentos=totalAssentos
+        self.totalLeitos=totalLeitos
+        self.empresa=empresa
+
+
+if __name__ == "__main__":
+    rSp = Rodoviaria(cidade="Sao Paulo")
+    rIta = Rodoviaria(cidade="Itajuba")
+    o = Onibus(empresa="Passaro Marron")
+    p = Passageiro(nome="Thiago")
+    b = Bilhete(origem=rSp, destino=rIta, onibus=o,passageiro=p)
+
+    print(b.passageiro.nome , "vai para", b.destino.cidade)
